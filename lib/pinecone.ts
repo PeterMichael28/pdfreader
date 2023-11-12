81866648e04be30a577f47ae3803b5938a5fb54c
peter michael <enitanpeters28@gmail.com>
2023-11-12 07:16:09 +0100

    pdfreader

---
+++ lib/pinecone.ts
@@ -0,0 +1,15 @@
+import { Pinecone } from '@pinecone-database/pinecone';
+
+export const getPineconeClient = async () => {
+  
+
+  const pinecone = new Pinecone({ 
+    apiKey: process.env.PINECONE_API_KEY!,
+    environment: 'us-east1-gcp',
+})
+
+  return pinecone
+};
+
+
+
